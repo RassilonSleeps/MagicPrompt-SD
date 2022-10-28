7f05175d2e89f3f2fbd77974e3bc7f4dfe23e97c
RassilonSleeps <64247429+RassilonSleeps@users.noreply.github.com>
2022-10-27 19:25:33 -0600

    batch scripts for install/launch
    add windows .bat files for dependency install and launch of python/jupyter/colab-local

---
+++ (launch)_colab-local-runtime.bat
@@ -0,0 +1,35 @@
+@echo off
+
+set PYTHON=python
+set VENV_DIR=venv
+
+:start_venv
+if [%VENV_DIR%] == [-] goto  :skip_venv
+
+dir %VENV_DIR%\Scripts\Python.exe
+if %ERRORLEVEL% == 0 goto :activate_venv
+
+for /f "delims=" %%i in ('CALL %PYTHON% -c "import sys; print(sys.executable)"') do set PYTHON_FULLNAME="%%i"
+echo Creating venv in directory %VENV_DIR% using python %PYTHON_FULLNAME%
+%PYTHON_FULLNAME% -m venv %VENV_DIR%
+if %ERRORLEVEL% == 0 goto :activate_venv
+echo Unable to create venv in directory %VENV_DIR%
+
+:activate_venv
+set PYTHON="%~dp0%VENV_DIR%\Scripts\python.exe"
+echo venv %PYTHON%
+goto :launch
+
+:skip_venv
+
+:launch
+call %VENV_DIR%\Scripts\activate.bat
+%PYTHON% -m pip install -r notebook-requirements.txt | findstr /V /C:"Requirement already satisfied"
+%PYTHON% -m jupyter notebook --no-browser --NotebookApp.allow_origin="https://colab.research.google.com" --port=8888 --NotebookApp.port_retries=0
+exit
+
+:endofscript
+echo.
+echo Launch unsuccessful. Exiting.
+pause
+
